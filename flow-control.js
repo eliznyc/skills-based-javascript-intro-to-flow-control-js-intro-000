0f4ab6c601fe85585547d743a5b3d47bbf707880
Elizabeth Peters <eliznyc@gmail.com>
2019-08-23 03:46:48 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,7 +13,11 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+if (age<13) {
+  return "You are a kid"} else if (age >19) {
+    return "You are a grownup"} else {
+      return "You are a teenager!"
+    }
 }
 
 function ternaryTeenager(age) {
