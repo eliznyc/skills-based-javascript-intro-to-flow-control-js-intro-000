7e88934cdd84f78db5db0ffb246daf300af76d3c
Elizabeth Peters <eliznyc@gmail.com>
2019-08-23 04:04:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,20 +27,20 @@ return (age>=13 && age <=19) ?
 
 function switchAge(age) {
 switch(age) {
-  case (13) : console.log("You are a teenager");
+  case 13 : "You are a teenager";
   break;
-  case (14) :  console.log("You are a teenager");
+  case 14 :  "You are a teenager";
   break;
-  case (15) :  console.log("You are a teenager");
+  case 15 :  "You are a teenager";
   break;
-  case (16) :  console.log("You are a teenager");
+  case 16 :  "You are a teenager";
   break;
-  case (17) :  console.log("You are a teenager");
+  case 17 :  "You are a teenager";
   break;
-  case (18) :  console.log("You are a teenager");
+  case 18 :  "You are a teenager";
   break;
-  case (19) :  console.log("You are a teenager");
+  case 19 :  "You are a teenager";
   break;
-default:  console.log("You have an age");
+default: "You have an age";
 }
 }
